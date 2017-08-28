cecca765e1e93f1ca976f4eb873847dae1c0448d
Shawn Sherwood <shawn.sherwood@gmail.com>
2017-08-28 19:01:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Boot with Angular Starter
+
+Sample project setup of a Spring Boot application serving an Angular application.
+
+## TODO
+
+- Add Spring Security/OAuth2
---
+++ server/src/main/java/io/undertree/BootServerApplication.java
@@ -0,0 +1,11 @@
+package io.undertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootServerApplication.class, args);
+	}
+}
